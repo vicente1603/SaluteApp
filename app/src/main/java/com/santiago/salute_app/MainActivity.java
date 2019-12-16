dc862173d13724b9cbfa9ee5bfdec6f7fe65df70
Vicente José Santiago Costa Oliveira <VJSCOliveira@ssp.riges>
2019-12-16 10:29:27 -0300

    correções no layout dos botões

--- app/src/main/java/com/santiago/salute_app/MainActivity.java
+++ app/src/main/java/com/santiago/salute_app/MainActivity.java
@@ -10,6 +10,8 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -180,5 +182,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.sair) {
+
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
